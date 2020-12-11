ef4f30b7e3c0d667c21331fdb12cb4f4570d28cb
Sara Cemal <you@example.com>
2020-12-10 19:27:08 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Sara is the champion"
+
+// newHeader is the element (header), and victory is the id class, 
+// and inner HTML is what the header is going to display
