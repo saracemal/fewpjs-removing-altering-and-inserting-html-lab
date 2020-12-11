3571b80eb5f62d6f2cd84a4a26bcd8920b458b5d
Sara Cemal <you@example.com>
2020-12-10 19:23:52 -0500

    commit tree c1dfd6523b15235cf037903205ed4c01c1c81f9d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Sara is the champion"
